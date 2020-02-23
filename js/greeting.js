83485c0233c0686d5093717396bf7508c829cf29
librarYKim <yklikesyou@gmail.com>
2020-02-23 17:26:38 +0900

    change an avatar and font for Korean

---
+++ js/greeting.js
@@ -0,0 +1,44 @@
+const nameContainer = document.querySelector(".js-name");
+
+function paintName(name) {
+  nameContainer.innerHTML = "";
+  const title = document.createElement("span");
+  title.className = "name__text";
+  title.innerHTML = `Thanks for visting my blog, ${name} :)`;
+  nameContainer.appendChild(title);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // defalut 동작을 막음 - enter해도 사이트 이상해지지 않는거
+  const form = event.target;
+  const input = form.querySelector("input");
+  const value = input.value;
+  localStorage.setItem("username", value);
+  paintName(value);
+}
+
+function paintInput() {
+  const input = document.createElement("input");
+  input.placeholder = "Type your name here";
+  input.type = "text";
+  input.className = "name__input";
+  const form = document.createElement("form");
+  form.addEventListener("submit", handleSubmit); //submit event있으면 handleSubmit() 실행하라
+  form.appendChild(input);
+  nameContainer.appendChild(form);
+}
+
+function loadName() {
+  const name = localStorage.getItem("username");
+  if (name === null) {
+    paintInput();
+  } else {
+    paintName(name);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
